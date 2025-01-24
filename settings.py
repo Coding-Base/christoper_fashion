2b2ec00032592b9ed829030a0d2c84ff923b098c
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-01-24 23:49:40 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'shop_backend.wsgi.application'
 
 # Database Configuration
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    # 'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
 }
 
 # Password Validators
